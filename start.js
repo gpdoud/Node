80d749f774fbf3419f7ef38e20f52f6d39418ffb
Greg Doud <gpdoud@gmail.com>
2020-11-23 21:55:59 -0500

    ""

--- start.js
+++ start.js
@@ -1,11 +1,9 @@
-const p = (n) => { 
-    return new Promise((resolve, reject) => {
-        if(n % 2 == 0)
-            resolve(n);
-        else 
-            reject(n);
-    })
-};
+const p = (n) => { return new Promise((resolve, reject) => {
+    if(n % 2 == 0)
+        resolve(n);
+    else 
+        reject(n);
+})};
 p(2).then(
     res => console.log(res, "is even"),
     err => console.log(err, "is odd")
